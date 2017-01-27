929fbc00c4821eee64b6ac6bf6edad2dc6430ed4
jeff <jeff>
2017-01-27 09:42:08 -0800

    heroku mongo

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     secret   : "foobarbaz",
-    database : "mongodb://localhost:27017/fcc_vote"
+    //database : "mongodb://localhost:27017/fcc_vote",
+    database : "mongodb://heroku_9fhs1x0p:n98sc4jkqvs2fj6la97427ia6t@ds033259.mlab.com:33259/heroku_9fhs1x0p"
 }
